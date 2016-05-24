7dfbdd15b2f71ea90b01ec945392df9b11503651
Ulf Ekstrom <uekstrom@gmail.com>
2016-05-24 14:08:12 +0200

    Separate benchmark and test programs.

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
+#include <ctime>
+#include <cassert>
 using namespace std;
 #include "numvec.hpp"
-
-
-template<typename T>
-void f1(T &res, const T &x)
-{
-  T u,y = 2*x;
-  y = 7*x;
-  y -= 1.2;
-  u = x + y;
-  y = y/x;
-  u = 3/x;
-  u += -y;
-  u += 1.1*x;
-  res = sin(u);
-}
 
 void bench_double(double *dst, const double *src, size_t len)
 {
   for (size_t i = 0; i < len; i++)
-    dst[i] = sin(cos(src[i]));
+    dst[i] = exp(cos(src[i])) + 1.8*src[i];
 }
 
 // At blocksize = 4 this is already faster than the double version.
@@ -35,22 +22,34 @@ void bench_numvec(double *dst, const double *src, size_t len)
       const numvec<double,blocksize> *ps = reinterpret_cast< const numvec<double,blocksize>* > (src + i*blocksize);
       numvec<double,blocksize> *pd = reinterpret_cast< numvec<double,blocksize>* > (dst + i*blocksize);
       tmp = cos(*ps);
-      *pd = sin(tmp);
+      *pd = exp(tmp);
+      *pd += 1.8*(*ps);
     }
 }
 
 
 int main(int argc, const char *argv[])
 {
-  const int len = 1<<27;
+  const int len = 1<<25;
+  const int blocksize = 128;
   double *x = new double[len];
-  double *y = new double[len];
+  double *y = new double[len];  
   for (int i=0;i<len;i++)
     x[i] = i % 10 + 0.1;
-  bench_double(y,x,len);
-  //bench_numvec<4>(y,x,len);
-  //  bench_numvec<>(y,x,len);
-  cout <<  y[len-1] << endl;
+  for (int i=0;i<len;i++)
+    y[i] = 0;
+  cout << "Main loop starting" << endl;
+  for (int i=0;i<10;i++)
+    {
+      clock_t tick = clock();
+      bench_double(y,x,len);
+      clock_t tock = clock();
+      cout << y[len-1] << " Standard loop Time    : " << (tock - tick)/(double)CLOCKS_PER_SEC << endl;
+      tick = clock();
+      bench_numvec<blocksize>(y,x,len);
+      tock = clock();
+      cout << y[len-1] << " Block<" << blocksize << "> vector Time: " << (tock - tick)/(double)CLOCKS_PER_SEC << endl;
+    }
   delete[] x;
   delete[] y;
   return 0;
---
+++ correctness.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+#include "numvec.hpp"
+
+
+template<typename T>
+void f1(T &res, const T &x)
+{
+  T u,y = 2*x;
+  y = 7*x;
+  y -= 1.2;
+  u = x + y;
+  y = y/x;
+  u = 3/x;
+  u += -y;
+  u += 1.1*x;
+  res = sin(u);
+}
+
+void test_correctness()
+{
+  const int len = 4;
+  numvec<double, len> inp, out;
+  for (int i=0;i<len;i++)
+    inp[i] = i+0.1;
+  f1(out,inp);
+  double sumres = 0;
+  for (int i=0;i<len;i++)
+    {
+      double outd;
+      f1(outd,inp[i]);
+      sumres += fabs(out[i] - outd);
+      cout << outd << " " << out[i] << endl;
+    }
+  cout.precision(15);
+  cout << "Sum of absolute differences " << sumres << endl;
+}
+
+
+int main(int argc, const char *argv[])
+{
+  test_correctness();
+  return 0;
+}
